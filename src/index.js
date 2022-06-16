2d3e3ddede16e659d214ea28a28764a67288bf3b
Adityavijaysingh2611 <75929995+Adityavijaysingh2611@users.noreply.github.com>
2022-06-16 20:18:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { DarkModeContextProvider } from "./context/darkModeContext";
-import Login from "./pages/login/Login";
 
 ReactDOM.render(
   <React.StrictMode>
