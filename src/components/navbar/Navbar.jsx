d7a6346581739a3e0bd353e5066bb8ba9d4079f3
Adityavijaysingh2611 <75929995+Adityavijaysingh2611@users.noreply.github.com>
2022-06-16 20:17:01 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,6 @@ import { DarkModeContext } from "../../context/darkModeContext";
 import { useContext } from "react";
 
 const Navbar = () => {
-  const { dispatch } = useContext(DarkModeContext);
 
   return (
     <div className="navbar">
